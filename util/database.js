979b11900671017d75d1dd0e17218577229ac191
Sanjeev <knit16643@gmail.com>
2021-10-27 15:38:34 +0530

    Ecommerece with  Mongoose ORM

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node-complete', 'root', '123456', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
